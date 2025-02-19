641c739a035eb190a4bc1558c7cb5b40562b6ba9
bjacob27 <b.jacobo@wustl.edu>
2025-02-19 14:22:11 -0600

    :)

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -2,10 +2,31 @@ package studio4;
 
 import java.awt.Color;
 
-import edu.princeton.cs.introcs.StdDraw;
 
+import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Image;
 public class Flag {
 	public static void main(String[] args) {
+		Color color = new Color (255, 57, 175); 
+		StdDraw.setPenColor(color);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.3); 
+		//first 2 is x an y (center) second two hor far from center
 		
+		Color color2 = new Color (255, 255, 255); 
+		StdDraw.setPenColor(color2);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.02);
+		StdDraw.filledRectangle(0.16, 0.25, 1, 0.02);
+		StdDraw.filledRectangle(0.16, 0.375, 1, 0.02);
+		StdDraw.filledRectangle(0.16, 0.75, 1, 0.02);
+		StdDraw.filledRectangle(0.16, 0.625, 1, 0.02);
+		
+		//Color color3 = new Color (255, 255, 255); 
+		//StdDraw.setPenColor(color3);
+		//StdDraw.filledRectangle(0.5, 0.5, 0.35, 0.3);
+		
+		StdDraw.setXscale(0 , 1); 
+		StdDraw.setYscale(0 , 1); 
+		 StdDraw.picture(0.5, 0.5, "resources/png-clipart-jacob-elordi-je-2.png");
+			StdDraw.show(); 
+		}
 	}
-}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,38 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next(); 
+		int r = in.nextInt(); 
+		int g = in.nextInt();
+		int b = in.nextInt();
+		boolean filled = in.nextBoolean();
+		double p1 = in.nextDouble(); 
+		double p2 = in.nextDouble();
+		double p3 = in.nextDouble();
+		double p4 = in.nextDouble();
+		
+		 
+		if (shape .equals("ellipse") && filled == false) {
+			Color color = new Color (r, g, b);  
+			StdDraw.setPenColor(color);
+			StdDraw.ellipse(p1, p2, p3, p4);
+		}
+		else if (shape .equals ("ellipse") && filled == true) {
+			Color color = new Color (r, g, b);
+			StdDraw.setPenColor(color);
+			StdDraw.filledEllipse(p1, p2, p3, p4);
+		}
+		else if (shape .equals ("rectangle") && filled == false) {
+			Color color = new Color (r, g, b);
+			StdDraw.setPenColor(color);
+			StdDraw.rectangle(p1, p2, p3, p4);
+		}
+		else if (shape .equals ("rectangle") && filled == true) {
+			Color color = new Color (r, g, b);
+			StdDraw.setPenColor(color);
+			StdDraw.filledRectangle(p1, p2, p3, p4);
+		}
+		
 		
 	}
 }
